9f0da58ab900235156ecb73999f38ee5490ff832
Om-Singh-1 <114053628+Om-Singh-1@users.noreply.github.com>
2022-09-28 21:36:21 +0530

    Added AssignmentTwo answers.

---
+++ AssignmentTwo/src/com/exceptionHandling/Q1/DivisionByZero.java
@@ -0,0 +1,26 @@
+package com.exceptionHandling.Q1;
+
+public class DivisionByZero {
+
+	  static void divideByZero(int num1, int num2) {
+		  
+		  try {
+			  
+			  int ans = num1/num2;
+			  System.out.println(ans);
+			  
+		  } catch (Exception E) {
+			  
+			  System.out.println("Error: " + E.getLocalizedMessage());
+			  
+		  }
+		  
+	  }
+	  
+	  public static void main(String[] args) {
+		  
+		  divideByZero(10, 0);
+		  
+	  }
+
+}
